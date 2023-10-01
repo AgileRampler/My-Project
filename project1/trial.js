d16a5752b0e8211f90de3cf703a7e479369a6e27
AgileRampler <43984000+AgileRampler@users.noreply.github.com>
2023-10-02 00:57:43 +0530

    Update trial.js

---
+++ project1/trial.js
@@ -0,0 +1,15 @@
+countEl = document.getElementById("count");
+
+let count = 0;
+
+function increment(){
+    count= count + 1;
+    countEl.innerText=count;
+    
+    
+}
+console.log("countEl")
+
+
+
+
