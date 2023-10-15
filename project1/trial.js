fed66564ed273d492f21e646f49995d95b19702c
AgileRampler <43984000+AgileRampler@users.noreply.github.com>
2023-10-16 01:30:08 +0530

    Update trial.js

--- project1/trial.js
+++ project1/trial.js
@@ -1,13 +1,18 @@
 countEl = document.getElementById("count");
-
+saveEl  = document.getElementById("save");
 let count = 0;
 
 function increment(){
-    count= count + 1;
+count = count + 1;
     countEl.innerText=count;
-    
-    
 }
+function save(){
+    let countStr = count + "-" ;
+    countEl.innerText += countStr;
+    countStr = 0;
+    count = 0;
+}
+    
 console.log("countEl")
 
 
